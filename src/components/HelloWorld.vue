055ec12f08a156e8a56e9a12ae938e5eb62f87ff
huangxutao <huangxutao@bosssoft.com.cn>
2019-08-10 14:22:30 +0800

    打印

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,138 @@
+<template>
+ <div id="hello">
+  <button @click="printDemo">测试打印</button>
+        <vue-easy-print tableShow ref="easyPrint" >
+            <template slot-scope="func">
+                <demo :getChineseNumber="func.getChineseNumber" ></demo>
+            </template>
+        </vue-easy-print>
+</div>
+</template>
+
+<script>
+import vueEasyPrint from "vue-easy-print";
+import demo from "./demo";
+export default {
+  name: "HelloWorld",
+  data() {
+    return {
+      tableData: {
+        id: 998,
+        store_name: "测试店铺",
+        created_at: "2018-06-06 15:21:35",
+
+        detail: [
+          {
+            name: "商品A",
+            item_unit: "40ML",
+            item_size: "瓶",
+            item_quantity: 5,
+            item_price: 188,
+            pv: 150,
+            item_total: 5 * 188,
+            item_total_pv: 5 * 150
+          },
+          {
+            name: "商品A",
+            item_unit: "40ML",
+            item_size: "瓶",
+            item_quantity: 5,
+            item_price: 188,
+            pv: 150,
+            item_total: 5 * 188,
+            item_total_pv: 5 * 150
+          },
+          {
+            name: "商品A",
+            item_unit: "40ML",
+            item_size: "瓶",
+            item_quantity: 5,
+            item_price: 188,
+            pv: 150,
+            item_total: 5 * 188,
+            item_total_pv: 5 * 150
+          },
+          {
+            name: "商品A",
+            item_unit: "40ML",
+            item_size: "瓶",
+            item_quantity: 5,
+            item_price: 188,
+            pv: 150,
+            item_total: 5 * 188,
+            item_total_pv: 5 * 150
+          },
+          {
+            name: "商品A",
+            item_unit: "40ML",
+            item_size: "瓶",
+            item_quantity: 5,
+            item_price: 188,
+            pv: 150,
+            item_total: 5 * 188,
+            item_total_pv: 5 * 150
+          },
+          {
+            name: "商品A",
+            item_unit: "40ML",
+            item_size: "瓶",
+            item_quantity: 5,
+            item_price: 188,
+            pv: 150,
+            item_total: 5 * 188,
+            item_total_pv: 5 * 150
+          },
+          {
+            name: "商品A",
+            item_unit: "40ML",
+            item_size: "瓶",
+            item_quantity: 5,
+            item_price: 188,
+            pv: 150,
+            item_total: 5 * 188,
+            item_total_pv: 5 * 150
+          },
+          {
+            name: "商品A",
+            item_unit: "40ML",
+            item_size: "瓶",
+            item_quantity: 5,
+            item_price: 188,
+            pv: 150,
+            item_total: 5 * 188,
+            item_total_pv: 5 * 150
+          }
+        ]
+      }
+    };
+  },
+  methods: {
+    printDemo() {
+      this.$refs.easyPrint.print();
+    }
+  },
+  components: {
+    vueEasyPrint,
+    demo
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1,
+h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
